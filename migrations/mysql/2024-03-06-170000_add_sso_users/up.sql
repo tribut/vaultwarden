eae08309132ecac5a708c24a6c7482bfe286ac64
Timshel <Timshel@users.noreply.github.com>
2024-04-01 15:41:58 +0200

    Fix mysql sso_users.identifier key creation error

--- migrations/mysql/2024-03-06-170000_add_sso_users/up.sql
+++ migrations/mysql/2024-03-06-170000_add_sso_users/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE sso_users (
   user_uuid           CHAR(36) NOT NULL PRIMARY KEY,
-  identifier          TEXT NOT NULL UNIQUE,
+  identifier          VARCHAR(768) NOT NULL UNIQUE,
   created_at          TIMESTAMP NOT NULL DEFAULT now(),
 
   FOREIGN KEY(user_uuid) REFERENCES users(uuid)
